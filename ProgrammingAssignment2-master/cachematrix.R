4d812b21dfc5a070dc4702b2c0d85df2e71c445c
OscarPorras <porrasjr_oscar@yahoo.com>
2017-12-03 16:02:56 +0800

    Commit

---
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -0,0 +1,34 @@
+## Program that call a function that computes inverse, retrieves the inverse
+## and return to original form
+
+##Creating Special Matrix
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+
+## Program that assumes that matrix can be inverted
+
+cacheSolve <- function(x, ...) {
+  ## Return a Matrix that is the inverse value of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+}
